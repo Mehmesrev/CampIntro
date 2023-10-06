e6f1eda45210a9fb1624d0e4d384aa6f525138b8
Mehmesrev <mehmesrev@gmail.com>
2023-10-06 12:19:57 +0300

    Yazılım Geliştirici Yetiştirme Kampı (C# + ANGULAR) isimli kursun dördüncü gününün altıncı ödevi

---
+++ YGYK4_5/MyDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YGYK4_5
+{
+    class MyDictionary<Key, Value> //METOT CLASS
+    {
+        Key[] _keys;
+        Value[] _values;
+
+        public MyDictionary()
+        {
+            _keys = new Key[0];
+            _values = new Value[0];
+        }
+
+        //EKLEME METODU
+        public void Add(Key key, Value value)
+        {
+            Key[] tempKeys = _keys;
+            Value[] tempValues = _values;
+
+            _keys = new Key[_keys.Length + 1];
+            _values = new Value[_values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                _keys[i] = tempKeys[i];
+                _values[i] = tempValues[i];
+            }
+
+            _keys[_keys.Length - 1] = key;
+            _values[_values.Length - 1] = value;
+        }
+
+        //
+        public int Length
+        {
+            get { return _keys.Length; }
+        }
+
+        public Key[] Keys
+        {
+            get { return _keys; }
+        }
+
+        public Value[] Values
+        {
+            get { return _values; }
+        }
+    }
+}
+
--- YGYK4_5/Program.cs
+++ YGYK4_5/Program.cs
@@ -1,20 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using YGYK4_5;
+
+using System;
 
 namespace YGYK4_5
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
-        {
+        {   //MYDİCTİONARY İSİMLİ DİCTİONARY
             MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
 
+            //KEY-VALUE EKLEME
             myDictionary.Add(1, "Bir");
             myDictionary.Add(2, "İki");
             myDictionary.Add(3, "Üç");
 
-            // Şimdi anahtar-değer çiftlerini ekrana yazdırabilirsiniz
+            //LİSTEMELE
             for (int i = 0; i < myDictionary.Length; i++)
             {
                 Console.WriteLine("Anahtar: " + myDictionary.Keys[i] + ", Değer: " + myDictionary.Values[i]);
