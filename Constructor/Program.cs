c5dc2856ac33d10a5be89b404bd3a14ed3bef3d2
Mehmesrev <mehmesrev@gmail.com>
2023-10-10 16:41:45 +0300

    CTOR UPDATED

--- Constructor/Program.cs
+++ Constructor/Program.cs
@@ -12,22 +12,22 @@ namespace Constructor
                 customer2.FirstName = "Semih";
                 customer2.LastName = "V";  
 
-            //SPECİAL CONSTRUCTOR
+            //ÖZEL CONSTRUCTOR
             Customer customer3 = new Customer(3, "Yusuf", "V");
-            //special constructor ile customer eklemenin ikinci yolu ama bunun için (x) kısmı yazılmalı
+            //özel constructor ile customer eklemenin ikinci yolu ama bunun için (x) kısmı yazılmalı
             customer3.Id = 3;
             customer3.FirstName = "Yusuf";
             customer3.LastName = "V";
         }
     }
-    class Customer
+    class Customer //BU CLASS, AYNI İSİMDE İÇİNDE OLUŞTURULURSA CTOR
     {
         public Customer() //DEFAULT CONSTRUCTOR 
         {
             Console.WriteLine("default constructor çalıştı.");
         }
 
-        public Customer(int id, string firstName, string lastName) //SPECİAL CONSTRUCTOR
+        public Customer(int id, string firstName, string lastName) //ÖZEL CONSTRUCTOR
         {   //(x) proplar bu şekilde parametreler ile eşleştrilmeli
             Id = id;
             FirstName = firstName;
